452f962a21edb27ee80c9cbb407e526783d1dcab
Luiz Pontes <eldorado.luiz@gmail.com>
2019-03-15 22:02:49 -0300

    Fixed excessive spacing in new project

--- android-new-project.py
+++ android-new-project.py
@@ -31,69 +31,69 @@ def main(args):
 	with open(args.name+R"""\src\\"""+package_dir+R"""\MainActivity.java""", 'w') as f:
 		f.write(R"""package """+ args.package +R""";
 
-	import android.app.Activity;
-	import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
 
-	public class MainActivity extends Activity {
-		@Override
-		protected void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			setContentView(R.layout.activity_main);
-		}
-	}""")
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}""")
 
 	print('Making '+R"""res\values\strings.xml...""")
 
 	with open(args.name+R"""\res\values\strings.xml""", 'w') as f:
 		f.write(R"""<resources>
-		<string name="app_name">"""+ args.name +R"""</string>
-		<string name="hello_world">Hello, world!</string>
-	</resources>""")
+	<string name="app_name">"""+ args.name +R"""</string>
+	<string name="hello_world">Hello, world!</string>
+</resources>""")
 
 	print('Making '+R"""res\layout\activity_main.xml""")
 
 	with open(args.name+R"""\res\layout\activity_main.xml""", 'w') as f:
 		f.write(R"""<RelativeLayout
-		xmlns:android="http://schemas.android.com/apk/res/android" xmlns:tools="http://schemas.android.com/tools"
-		android:layout_width="match_parent"
-		android:layout_height="match_parent"
-		>
-		
-		<TextView
-			android:layout_width="wrap_content"
-			android:layout_height="wrap_content"
-			android:layout_centerHorizontal="true"
-			android:layout_centerVertical="true"
-			android:text="@string/hello_world"
-			tools:context=".MainActivity"
-			/>
+	xmlns:android="http://schemas.android.com/apk/res/android" xmlns:tools="http://schemas.android.com/tools"
+	android:layout_width="match_parent"
+	android:layout_height="match_parent"
+	>
+	
+	<TextView
+		android:layout_width="wrap_content"
+		android:layout_height="wrap_content"
+		android:layout_centerHorizontal="true"
+		android:layout_centerVertical="true"
+		android:text="@string/hello_world"
+		tools:context=".MainActivity"
+		/>
 
-	</RelativeLayout>""")
+</RelativeLayout>""")
 
 	print('Making AndroidManifest.xml...')
 
 	with open(args.name+R"""\AndroidManifest.xml""", 'w') as f:
 		f.write(R"""<?xml version='1.0'?>
 
-	<manifest
-		xmlns:a='http://schemas.android.com/apk/res/android'
-		package='"""+ args.package +R"""'
-		a:versionCode='1'
-		a:versionName='1'
-		>
-		<application a:label='"""+ args.name +R"""'>
-			<activity a:name='"""+ args.package +R""".MainActivity'>
-				<intent-filter>
-					<category a:name='android.intent.category.LAUNCHER'/>
-					<action a:name='android.intent.action.MAIN'/>
-				</intent-filter>
-			</activity>
-		</application>
+<manifest
+	xmlns:a='http://schemas.android.com/apk/res/android'
+	package='"""+ args.package +R"""'
+	a:versionCode='1'
+	a:versionName='1'
+	>
+	<application a:label='"""+ args.name +R"""'>
+		<activity a:name='"""+ args.package +R""".MainActivity'>
+			<intent-filter>
+				<category a:name='android.intent.category.LAUNCHER'/>
+				<action a:name='android.intent.action.MAIN'/>
+			</intent-filter>
+		</activity>
+	</application>
 
-	</manifest>""")
+</manifest>""")
 
 	print('Done.')
 
 if __name__ == "__main__":
 
-	main(sys.argv)
+	main(sys.argv[1:])
